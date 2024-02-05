688815074e6e0c247a6d8764553bf72f95fd8dbe
ballboi21 <157715337+ballboi21@users.noreply.github.com>
2024-02-05 13:52:03 +0000

    done

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,23 @@
+sport = intput("gimmie a jucie sport: ")
+liquid = intput("gimmie a liquid: ")
+gas = intput("gimmie a smelly gas: ")
+solid = intput("gimmie a smelly sollid: ")
+verb = intput("gimmie a verb: ")
+color = intput("gimmie a color: ")
+soccer_player = intput("gimmie a soccer_player: ")
+class_in school = intput("gimmie a class in school: ")
+president = input("gimmie a president: ")
+
+print("i play",sport"i drink",liquid
+"i breath in",gas"i eat",solid"i",verb
+"i see",color"on",soccer_player
+"shirt and he is in",class_in school,"and the theacher is",president)
+
+
+
+
+
+
+
+
 
